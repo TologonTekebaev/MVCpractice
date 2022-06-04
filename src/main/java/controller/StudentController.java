6da310f0518e79cda2785e80be109d4cc0cbffd1
user <tologon.tkbv@gmail.com>
2022-06-05 02:28:19 +0600

    pratice

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -4,9 +4,12 @@ import entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import service.StudentService;
+
+import java.util.List;
 
 /**
  * @author: Tologon Tekebaev
@@ -19,14 +22,21 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-    @PostMapping("/saveStudent")
+
+    public String getAllStudents(Model model ){
+        List<Student> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+        return " ";
+    }
+    @PostMapping("/addStudent")
     public String addCompany(Model model){
          model.addAttribute("student", new Student());
          return "student/addStudent";
     }
-    
+
+    @GetMapping("/saveStudent")
     public String saveStudent(@ModelAttribute("student") Student student){
         studentService.addStudent(student);
-        return "redirect:ge";
+        return "redirect:getAllStudents";
     }
 }
